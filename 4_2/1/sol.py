c87d799059516e502d9622c70f16e8c423ad6d5e
tyraello <kaplanov.v@gmail.com>
2018-11-21 07:43:06 +0300

    huffman encode

--- 4_2/1/sol.py
+++ 4_2/1/sol.py
@@ -1,21 +1,47 @@
-from collections import Counter
 import sys
+import heapq
+from collections import Counter, namedtuple
+
+
+class Node(namedtuple("Node", ["left", "right"])):
+    def walk(self, code, acc):
+        self.left.walk(code, acc+"0")
+        self.right.walk(code, acc+"1")
+
+
+class Leaf(namedtuple("Leaf", ["char"])):
+    def walk(self, code, acc):
+        code[self.char] = acc or "0"
 
 
 def read(inp):
     s = inp.readline()
-
     return s
 
 
 def huffman(s):
-    symbols = Counter(s)
+    h = []
+    for ch, freq in Counter(s).items():
+        h.append((freq, len(h), Leaf(ch)))
+    # h = [(freq, Leaf(ch)) for ch, freq in Counter(s).items()]
+    heapq.heapify(h)
 
-    return len(symbols), symbols
+    count = len(h)
+    while len(h) > 1:
+        freq1, _c1, left = heapq.heappop(h)
+        freq2, _c2, right = heapq.heappop(h)
+        heapq.heappush(h, (freq1 + freq2, count, Node(left, right)))
+        count += 1
+
+    code = {}
+    if h:
+        [(_freq, _, root)] = h
+        root.walk(code, "")
+    return code
 
 
-def encode(string, codes):
-    return ''.join(str(bin(codes[c])[2:]) for c in string)
+def encode(string, code):
+    return ''.join(code[c] for c in string)
 
 
 if __name__ == '__main__':
@@ -24,9 +50,10 @@ if __name__ == '__main__':
 
     n = read(inp)
 
-    cnt, codes = huffman(n)
+    codes = huffman(n)
     res = encode(n, codes)
 
-    print(cnt)
-    print(' '.join([f'{k}: {v}' for k, v in codes.items()]))
+    print(len(codes), len(res))
+    for c in sorted(codes):
+        print(f'{c}: {codes[c]}')
     print(res)
