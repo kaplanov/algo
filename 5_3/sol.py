4e317b4c943b01b5ab3568bd3b84ba9c16e72678
tyraello <kaplanov.v@gmail.com>
2018-12-04 09:13:10 +0300

    quick sort points divide finished

--- 5_3/sol.py
+++ 5_3/sol.py
@@ -18,23 +18,25 @@ def swap(arr, i, j):
 
 def quick_lines(point_list, line_list, beg, end, beg_lines, end_lines, res_dict):
     if beg >= end:
-        res_dict[point_list[end]] = end
+        point = point_list[end]
+        cnt, line_piv_1, line_piv_2 = partition_lines(0, len(line_list) - 1, point, line_list)
+        res_dict[point] = cnt
         return
 
     new_pivot = partition(beg, end, point_list)
 
-    cnt, line_piv_1, line_piv_2 = partition_lines(beg_lines, end_lines, point_list[new_pivot], line_list)
-    res_dict[point_list[new_pivot]] = new_pivot
+    # cnt, line_piv_1, line_piv_2 = partition_lines(beg_lines, end_lines, point_list[new_pivot], line_list)
+    cnt, line_piv_1, line_piv_2 = partition_lines(0, len(line_list)-1, point_list[new_pivot], line_list)
+    res_dict[point_list[new_pivot]] = cnt
 
     quick_lines(point_list, line_list, beg, new_pivot - 1, beg_lines, line_piv_1 - 1, res_dict)
-    quick_lines(point_list, line_list, new_pivot + 1, end, line_piv_2 + 1, end_lines, res_dict)
+    quick_lines(point_list, line_list, new_pivot + 1, end, line_piv_2, end_lines, res_dict)
 
 
 def partition_lines(beg, end, point, line_list):
-    line_cnt = 0
-    pivot1 = 0
-    pivot2 = 0
     i = j = k = 0
+    if beg >= end:
+        return 0, 0, 0
     while i <= end and j <= end and k <= end:
         line = line_list[k]
         if line[1] < point:
@@ -44,22 +46,22 @@ def partition_lines(beg, end, point, line_list):
                 j = i
             if k < i:
                 k = i
-        elif line[0] <= point <= line[1]:
+        line2 = line_list[k]
+        if line2[0] <= point <= line2[1]:
             swap(line_list, j, k)
             j += 1
-            if j < i:
-                j = i
-            if k < i:
-                k = i
+            if k < j:
+                k = j
+        line3 = line_list[k]
+        if line3[0] > point:
+            k += 1
 
-
-
-    return line_cnt, pivot1, pivot2
+    return j - i, i, j
 
 
 def partition(beg, end, point_list):
     pivot = beg
-    swap(point_list, pivot, randint(beg, end))
+    # swap(point_list, pivot, randint(beg, end))
 
     i = j = beg + 1
     while j <= end and i <= end:
@@ -76,7 +78,7 @@ def partition(beg, end, point_list):
 
 
 if __name__ == '__main__':
-    inp = open('2', 'r')
+    inp = open('1', 'r')
     # inp = sys.stdin
 
     lines, points = read_array(inp)
@@ -85,7 +87,7 @@ if __name__ == '__main__':
     print(lines)
     print(points)
     res = {}
-    quick_lines(points, lines, 0, len(points) - 1, 0, len(points) - 1, res)
+    quick_lines(points, lines, 0, len(points) - 1, 0, len(lines) - 1, res)
 
     print('results')
     print(points)
